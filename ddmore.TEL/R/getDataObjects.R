fbf8de48bf9db4fd74071130235b19d6fc7ea281
khanley <khanley@mango-solutions.com>
2014-06-19 15:55:02 +0100

    DDMORE-542 - Add files for getDataObjects function and dataObj class

---
+++ ddmore.TEL/R/getDataObjects.R
@@ -0,0 +1,69 @@
+##############################################################
+#' getDataObjects
+#'
+#' Retrieves Data Object(s) (MCL object of class “dataObj”) from a locally stored 
+#' MDL file or from a URL and returns an S4 object of class “dataObj”. 
+#' Slots within this object are  “DATA_INPUT_VARIABLES”, “SOURCE”, “DATA_DERIVED_VARIABLES” 
+#' and “DESIGN” which are named lists of lists containing parsed information from 
+#' the relevant MCL Data Object blocks and, optionally, the “RAW” vector of 
+#' character strings corresponding to the lines of MCL code within the MCL Data Object.
+#'
+#' @usage getDataObjects(file, name, raw=F)
+#'
+#' @param file File path or URL of the .mdl file containing the data object.
+#'
+#' @param name – (Optional) Specifies the data object item, by name, to be 
+#' retrieved by getDataObjects. If multiple data objects exist in the .mdl file 
+#' then using the name argument helps users target a specific data object. 
+#'
+#' @details
+#' getDataObjects only retrieves the MCL code, it does not read any data file 
+#' identified within the “SOURCE” block. A "read" method should be applied to 
+#' the resulting object which interprets the SOURCE, FILE and HEADER information 
+#' and reads the specified data file into a data frame.
+#'
+#' @return Returns an S4 Object of class "dataObj" containing slots "RAW", 
+#' "DATA_INPUT_VARIABLES", "SOURCE", "DATA_DERIVED_VARIABLES" and "DESIGN".
+#'
+#' @export
+#' @docType methods
+#' @rdname getDataObjects-methods
+#'
+#' @examples
+#' ## Retrieve from the DDMoRe Library
+#' ThamDataObject <- getDataObjects(
+#'   file="http://ddmore.eu/model-repository/model/download/127.17?filename=2008ThamJCCR.mdl")
+#'
+#' ## Retrieve from a local file
+#' ThamDataObject <- getDataObjects("2008ThamJCCR.mdl") 
+#'
+#' ## Retrieve named data object from a local file
+#' tumourSizeDataObject <- getDataObjects("2008ThamJCCR.mdl",
+#'  				name="tumour_size_dat")
+#'
+#' ThamDataObject[[1]] ## first data object within ThamDataObject
+#'
+#' ## Using data object name from MCL code
+#' tumourSizeDataObject<-ThamDataObject$tumour_size_dat
+#'
+#' ## Using awk to change data Object text lines prior to parsing.
+#' ThamDataObject2<- getDataObjects(awk("myawkfile.awk", ThamDataObject@RAW))
+
+setGeneric("getDataObjects", function(file, name){ 
+  ## create object in R from parser:
+  #x <- .callParser(loc=file, obType="dataObject", obName=name) # should return a named list of objects
+  #return(x) # return the value
+  standardGeneric("getDataObjects")
+})
+
+#' @rdname getDataObjects-methods
+#' @aliases getDataObjects,mogObj,mogObj-method
+setMethod("getDataObjects", "mogObj", function(object){
+    return(object@dataObj)
+})
+
+
+
+
+
+
---
+++ ddmore.TEL/R/telClasses.R
@@ -0,0 +1,40 @@
+# $LastChangedDate: 2014-06-19 $
+# $LastChangedBy: khanley $
+# 
+# Author: khanley
+###############################################################################
+
+
+validity.NMProblem <- function(object)
+{
+	stopifnot(is.list(object$DATA_INPUT_VARIABLES))
+  stopifnot(is.list(object$SOURCE))
+  stopifnot(is.list(object$RSCRIPT))
+  stopifnot(is.list(object$HEADER))
+  stopifnot(is.list(object$FILE))
+  stopifnot(is.list(object$DESIGN))
+  stopifnot(is.vector(object$DATA_DERIVED_VARIABLES))
+  return(TRUE)
+}
+
+#' @slot DATA_INPUT_VARIABLES A named list
+#' @slot SOURCE A list of parsed sections of the control file
+#' @slot RSCRIPT TBC 
+#' @slot HEADER TBC
+#' @slot FILE String containing name of the data file
+#' @slot DESIGN  Named list of lists describing the design of the experiment
+#' @slot DATA_DERIVED_VARIABLES TBC - vector of strings?
+#' @author khanley
+
+setClass("dataObj", 
+  slots=c(
+    DATA_INPUT_VARIABLES="list",
+    SOURCE = "list",
+    RSCRIPT = "list",
+    HEADER = "list",
+    FILE = "list",
+    DESIGN = "list",
+    DATA_DERIVED_VARIABLES = "vector"
+    ), 
+  validity = validity.dataObj
+)
