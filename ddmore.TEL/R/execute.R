1de34e14a9b63b5b47634b568087571cfe855b70
khanley <khanley@mango-solutions.com>
2014-06-20 17:31:47 +0100

    Update estimate function to rename mclObj to MOGObj to match updated design

--- ddmore.TEL/R/execute.R
+++ ddmore.TEL/R/execute.R
@@ -1,22 +1,22 @@
 #' Estimate  
 #'
-#' Passes an MCLObject to the target software for execution.
+#' Passes an MOGObject to the target software for execution.
 #'
 #' @author Jonathan Chard
-#' @param MCLObject An object of class mclObj.
+#' @param MOGObject An object of class mogObj.
 #' @param target S string specifying the target software. Currently, possible targets are "NONMEM", "PsN" and "BUGS".
 #' @param subfolder Specify the name of a subfolder within the current working directory in which to store the results.
 #' @param collect Logical dictating if the results should be collected.
 #' @param addargs String specifying additional arguments to be passed to the target software.
-estimate <- function(MCLObject=NULL, target=NULL, subfolder=format(Sys.time(), "%Y%b%d%H%M%S"), collect=TRUE, addargs="" ) {
+estimate <- function(MOGObject=NULL, target=NULL, subfolder=format(Sys.time(), "%Y%b%d%H%M%S"), collect=TRUE, addargs="" ) {
   originalDirectory <- getwd()
   outputObject <- list()
   if(target=="NONMEM") {
-    outputObject = estimate.NM(modelfile=MCLObject, originalDirectory, addargs)
+    outputObject = estimate.NM(modelfile=MOGObject, originalDirectory, addargs)
   } else if( target=="PsN") {
-    outputObject = estimate.PsN(modelfile=MCLObject, originalDirectory, addargs)    
+    outputObject = estimate.PsN(modelfile=MOGObject, originalDirectory, addargs)    
   } else if(target=="BUGS") {
-    estimate.BUGS(MCLObject, addargs)    
+    estimate.BUGS(MOGObject, addargs)    
   } else {
 	stop(sprintf('Unrecognized target: %s.', target))
   }
@@ -63,4 +63,4 @@ execute.BUGS<-function(modelfile=NULL,addargs="",...){
   cat("Not supported")
 }
 
-#estimate(MCLObject="warf_PK_CONC.mdl", target="BUGS", addargs="...")
+#estimate(MOGObject="warf_PK_CONC.mdl", target="BUGS", addargs="...")
