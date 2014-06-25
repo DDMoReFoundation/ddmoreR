9d6eeba8093565320768359f9d772f9d90da3818
Matthew Wise <mwise@mango-solutions.com>
2014-06-25 18:14:08 +0100

    DDMORE-732: Temp fix re data files - copy all from src dir to work dir

--- ddmore.TEL/R/mdlUtils.R
+++ ddmore.TEL/R/mdlUtils.R
@@ -1,15 +1,16 @@
-TEL.getInputs <- function(modelfile=NULL){
+TEL.getInputs <- function(modelfile=NULL) {
   
   ## Parse the data object from the model file
 
   ## Get the input files
-  if(file_ext(modelfile)=="ctl") {
-	parsedCtl <- importNmMod(modelfile)
-	parsedCtl[[4]][[1]]$Data[1]
-    #c("warfpk.csv") 
+  if (file_ext(modelfile)=="ctl") {
+    parsedCtl <- importNmMod(modelfile)
+    parsedCtl[[4]][[1]]$Data[1]
   }
   else {
-    #c("warfarin_conc_pca.csv")
-    c()
+    # TODO: This is a temporary fix in order to get the data files copied.
+    # The correct fix would be to actually parse the MDL or PharmML model file in order to extract the data file references. 
+    dir(recursive=1)
   }
+  
 }
