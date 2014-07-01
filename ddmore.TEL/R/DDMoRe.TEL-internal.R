386c375c18a211ba4a4aedc6bc749e25814b4d67
Matthew Wise <mwise@mango-solutions.com>
2014-07-01 18:21:55 +0100

    DDMORE-542: Temporarily don't start FIS&MIF servers on R-TEL pkg load.

--- ddmore.TEL/R/DDMoRe.TEL-internal.R
+++ ddmore.TEL/R/DDMoRe.TEL-internal.R
@@ -1,14 +1,22 @@
+
+# TODO: Reinstate these?
+# The problem is that as part of building the package, the package will attempt
+# to be loaded. Since the building is not taking place within an SEE environment,
+# FIS home and MIF home won't be found, so starting the servers will throw an error,
+# and this will cause the build to fail.
+
+
 .onAttach <-
 function(x,y) {
-  TEL.startServer()
+    # TEL.startServer()
 }
 
 .onUnload <-
 function(x) {
-    TEL.safeStop()
-  }
+    # TEL.safeStop()
+}
 
 .onDetach <-
-  function(x) {
-    TEL.safeStop()
-  }
+function(x) {
+    # TEL.safeStop()
+}
