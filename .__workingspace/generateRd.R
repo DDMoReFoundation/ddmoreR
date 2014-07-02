2c60b89736f8d4ba01a042567ee3374751cf4b1f
Matthew Wise <mwise@mango-solutions.com>
2014-07-02 09:28:47 +0100

    DDMORE-542: Working directory shouldn't be checked in.

--- .__workingspace/generateRd.R
+++
@@ -1,8 +0,0 @@
-require(utils)
-require(methods)
-library(roxygen2)
-cmds = commandArgs(TRUE)
-pkgp = cmds[1]
-# avoid collate_roclet() - topological sort, very slow
-# avoid namespace_roclet() - we need to export everything
-roxygenize(pkgp,pkgp,roclets='rd')
