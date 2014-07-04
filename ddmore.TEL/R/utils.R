7a9648775b0932957bc302ef679d2361063c73da
Matthew Wise <mwise@mango-solutions.com>
2014-07-04 15:08:19 +0100

    DDMORE-542: Most of the work for writing out MCL/MOG objects to MDL.

--- ddmore.TEL/R/utils.R
+++ ddmore.TEL/R/utils.R
@@ -10,7 +10,7 @@
 #'
 #' @usage .callParser("myMDLFile")
 #'
-#' @param x File path or URL of the .mdl file containing the objects.
+#' @param f File path or URL of the .mdl file containing the objects.
 #'
 #' @param type String specifying the type of objects to extract. Possible values are
 #' "parobj", "taskobj", "dataobj" and "mdlobj"
@@ -22,14 +22,14 @@
 #' @return A list of objects which are contained in the MDL file or URL.
 #' @include telClasses.R
 
-.callParser <- function(x, name, type, HOST='localhost', PORT='9010') {
+.callParser <- function(f, name, type, HOST='localhost', PORT='9010') {
 
   if(!type%in%c("parobj", "taskobj", "dataobj", "mdlobj")){
     stop("Type specified is not one of 'parobj', 'taskobj', 'dataobj' or 'mdlobj'")
   }
   
   # Call parser and read in the JSON data
-  raw <- .callParser0(x, HOST, PORT);
+  raw <- .callParser0(f, HOST, PORT);
 
   if (!missing(name)) {
   
@@ -45,10 +45,10 @@
   
 }
 
-.callParser0 <- function(x, HOST='localhost', PORT='9010') {
+.callParser0 <- function(f, HOST='localhost', PORT='9010') {
 
   # Call parser and read in the JSON data:
-  cmd <- URLencode(paste0("http://", HOST, ":", PORT, "/readmdl?fileName=", normalizePath(x, winslash="/")))
+  cmd <- URLencode(paste0("http://", HOST, ":", PORT, "/readmdl?fileName=", normalizePath(f, winslash="/")))
 
   fromJSON(httpGET(cmd))[[1]]
 }
@@ -93,24 +93,6 @@
 
 }
 
-.extractMdlObj <- function(dat){
-  #Extract identifiers
-  logi <- sapply(dat, 
-    function(x){
-      x$identifier=="mdlobj"
-    }
-  )
-  
-  subList <- dat[logi]
-  
-  res <- lapply(subList, .createMdlObj)
-  
-  names(res) <- names(subList)
-  
-  return(res)
-
-}
-
 .extractDataObj <- function(dat){
   #Extract identifiers
   logi <- sapply(dat, 
@@ -122,6 +104,24 @@
   subList <- dat[logi]
   
   res <- lapply(subList, .createDataObj)
+  
+  names(res) <- names(subList)
+  
+  return(res)
+
+}
+
+.extractMdlObj <- function(dat){
+  #Extract identifiers
+  logi <- sapply(dat, 
+    function(x){
+      x$identifier=="mdlobj"
+    }
+  )
+  
+  subList <- dat[logi]
+  
+  res <- lapply(subList, .createMdlObj)
   
   names(res) <- names(subList)
   
@@ -181,26 +181,117 @@
     
     res <- new("mdlObj",
         # TODO: TBC - These need to be populated
-        MODEL_INPUT_VARIABLES = list(),
-        STRUCTURAL_PARAMETERS = character(),
-        VARIABILITY_PARAMETERS = character(),
-        GROUP_VARIABLES = character(),
-        RANDOM_VARIABLE_DEFINITION = list(),
-        INDIVIDUAL_VARIABLES = character(),
+        MODEL_INPUT_VARIABLES = list(dat$MODEL_INPUT_VARIABLES),
+        STRUCTURAL_PARAMETERS = dat$STRUCTURAL_PARAMETERS,
+        VARIABILITY_PARAMETERS = dat$VARIABILITY_PARAMETERS,
+        GROUP_VARIABLES = dat$GROUP_VARIABLES,
+        RANDOM_VARIABLE_DEFINITION = dat$RANDOM_VARIABLE_DEFINITION,
+        INDIVIDUAL_VARIABLES = dat$INDIVIDUAL_VARIABLES,
         MODEL_PREDICTION = new("modPred",
             ODE = character(),
             LIBRARY = character()
         ),
-        OBSERVATION = list()
+        OBSERVATION = list(dat$OBSERVATION)
     )
 
 }
 
 
 .createTaskObj <- function(){error("No functionality has been implemented to extract task objects")}
-  
-  
-  
+
+
+
+##############################################################
+#' .write.mclobj
+#'
+#' Takes in an instance of R class mogObj comprising a single instance of each of:
+#'  - dataObj class
+#'  - parObj class
+#'  - mdlObj class
+#'  - taskObj class
+#' and a specified file, and writes out the content of the MOG Object to that file as MDL.
+#' It is recommended that the file not have an extension, whereby the .mdl extension will be appended.
+#' 
+#' @usage .write.mclobj(myMogObj, 'C:/Users/username/mymodel')
+#'
+#' @param m instance of R class mogObj
+#' @param f file path to the .mdl file (optionally without the .mdl extension) that will be created
+#'
+#' @include telClasses.R
+
+.write.mclobj <- function(m, f, HOST='localhost', PORT='9010') {
+
+    if (!validity.mogObj(m)) {
+        stop("Object is not a valid MOG Object")
+    }
+    
+    parObjAsList <- list(
+      STRUCTURAL = m@parObj@STRUCTURAL,
+      PRIOR = m@parObj@PRIOR,
+      VARIABILITY = m@parObj@VARIABILITY,
+      identifier = "parobj"
+    )
+    
+    dataObjAsList <- list(
+        DATA_INPUT_VARIABLES = m@dataObj@DATA_INPUT_VARIABLES,
+        SOURCE = m@dataObj@SOURCE,
+        RSCRIPT = m@dataObj@RSCRIPT,
+        HEADER = m@dataObj@HEADER,
+        FILE = m@dataObj@FILE,
+        DESIGN = m@dataObj@DESIGN,
+        DATA_DERIVED_VARIABLES = m@dataObj@DATA_DERIVED_VARIABLES,
+        identifier = "dataobj"
+    )
+    
+    mdlObjAsList <- list(
+        MODEL_INPUT_VARIABLES = m@mdlObj@MODEL_INPUT_VARIABLES,
+        STRUCTURAL_PARAMETERS = m@mdlObj@STRUCTURAL_PARAMETERS,
+        VARIABILITY_PARAMETERS = m@mdlObj@VARIABILITY_PARAMETERS,
+        GROUP_VARIABLES = m@mdlObj@GROUP_VARIABLES,
+        RANDOM_VARIABLE_DEFINITION = m@mdlObj@RANDOM_VARIABLE_DEFINITION,
+        INDIVIDUAL_VARIABLES = m@mdlObj@INDIVIDUAL_VARIABLES,
+        MODEL_PREDICTION = list(
+            ODE = m@mdlObj@MODEL_PREDICTION@ODE,
+            LIBRARY = m@mdlObj@MODEL_PREDICTION@LIBRARY
+        ),
+        OBSERVATION = m@mdlObj@OBSERVATION,
+        identifier = "mdlobj"
+    )
+    
+    # TODO: Implement the Task object
+    taskObjAsList <- list(
+        #m@taskObj
+        identifier = "taskobj"
+    )
+    
+    json <- toJSON(list(list(
+        mymodel_task = taskObjAsList,
+        mymodel_par = parObjAsList,
+        mymodel_mdl = mdlObjAsList,
+        mymodel_dat = dataObjAsList
+    )))
+    
+    .write.mclobj0(json, f, HOST, PORT)
+}
+
+.write.mclobj0 <- function(json, f, HOST='localhost', PORT='9010') {
+
+    wreq <- toJSON(list(
+        fileName = normalizePath(f, winslash="/", mustWork=FALSE),
+        fileContent = json
+    ))
+
+
+    # Call parser and post the JSON data:
+    cmd <- URLencode(paste0("http://", HOST, ":", PORT, "/writemdl"))
+
+    res <- RCurl:::curlPerform(url = cmd, postfields = sprintf('%s%s','writeRequest=',wreq))
+
+}
+
+
+
+
 
 
 ##############################################################
