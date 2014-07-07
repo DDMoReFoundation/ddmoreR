226ee50dce575dc9c9cc2a796cf0c43658e60e8c
Matthew Wise <mwise@mango-solutions.com>
2014-07-07 11:50:32 +0100

    DDMORE-542: TEL.getInputs() should find just *.csv, in modelfile dir. Currently was wrong in that a) all files were being returned, b) this was recursive and so files within the "2014Jul07114540" etc. output directories were being picked up, and c) even if restricted to *.csv recursively, the data files within the output directories would still be picked up. This is a temporary workaround pending extracting the correct specific path(s) to the data file(s) from the model file.

--- ddmore.TEL/R/mdlUtils.R
+++ ddmore.TEL/R/mdlUtils.R
@@ -10,8 +10,10 @@ TEL.getInputs <- function(modelfile=NULL) {
   }
   else {
     # TODO: This is a temporary fix in order to get the data files copied.
-    # The correct fix would be to actually parse the MDL or PharmML model file in order to extract the data file references. 
-    dir(recursive=1)
+    # The correct fix would be to actually parse the MDL or PharmML model file in order to extract the data file references.
+    # Also, the data files cannot currently be under a subfolder. The recursive=FALSE can be switched to
+    # recursive=TRUE once the specific data file relative path(s) has been identified.
+    dir(pattern=".*\\.csv", recursive=FALSE)
   }
   
 }
