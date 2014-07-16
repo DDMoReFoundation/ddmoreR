42cd8e535d31e396938c9658255d7c2d61f130ec
Matthew Wise <mwise@mango-solutions.com>
2014-07-16 11:07:52 +0100

    DDMORE-719: Create script tel-dev.R that loads in all the TEL.R R files. This essentially loads in the functions that the DDMoRe.TEL library provides, but allows the R files to be easily modified and reloaded without having to re-build the library, hence useful for development.

---
+++ tel-dev.R
@@ -0,0 +1,24 @@
+##
+## Load in the R files so that the workflow functions can be modified on-the-fly,
+## rather than having to re-build the DDMoRE.TEL package every time a change is made.
+##
+
+scripts.dir <- paste0(dirname(sys.frame(1)$ofile), "/ddmore.TEL/R/")
+
+script.files = c(
+    "telClasses", "createMogObj",
+    "utils", "execute", "fileUtils", "mdlUtils", "conversion", "read", "update", "outputObject",
+    "getDataObjects", "getParameterObjects", "getModelObjects", "getTaskPropertiesObjects", "getMDLObjects",
+    "server"
+)
+
+sapply(script.files, function(s) {
+    script.file <- paste0(scripts.dir, s, ".R");
+    cat(c("Loading in", script.file, "...\n"))
+    source(script.file)
+})
+
+# Load required libraries
+library(rjson)
+library(RCurl)
+library(RNMImport)
