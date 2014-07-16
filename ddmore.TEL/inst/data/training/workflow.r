5f6466d4f70c2205e138d8756d230e8679809327
Matthew Wise <mwise@mango-solutions.com>
2014-07-16 10:27:43 +0100

    DDMORE-719: Update the workflow.R following recent changes: - task object being implemented albeit holding a single string - use absolute paths to model files for execution rather than having to change current directory - fix the writing of the modified MOG back out to MDL

--- ddmore.TEL/inst/data/training/workflow.r
+++ ddmore.TEL/inst/data/training/workflow.r
@@ -1,8 +1,7 @@
 # Course TEL workflow
 
-#set working directory
-#setwd("C:\\Users\\khanley\\Documents\\Projects\\ddmore\\TEL\\training")
-#setwd("C:/runtime-mdl-editor/testproj/mdl/tumour")
+# SET THIS: The path (either absolute, or relative to your current directory) to the MDL file
+modelFile <- "C:/runtime-mdl-editor/testproj/mdl/tumour/tumour_size_25June2014_OAM.mdl"
 
 
 ## Load TEL package and other useful packages
@@ -20,7 +19,7 @@ objects("package:DDMoRe.TEL")
 list.files()
 
 # Parse the MDL and create an R object containig the information:
-parsed <- getMDLObjects("tumour_size_25June2014_OAM.mdl")
+parsed <- getMDLObjects(modelFile)
 
 # Look at the names of the parsed objects
 names(parsed)
@@ -29,22 +28,25 @@ names(parsed)
 tumour_size_ORG_dat <- parsed$tumour_size_ORG_dat
 tumour_size_ORG_mdl <- parsed$tumour_size_ORG_mdl
 tumour_size_ORG_par <- parsed$tumour_size_ORG_par
+tumour_size_ORG_task <- parsed$tumour_size_ORG_task
 
 # Look at the type of objects:
 class(tumour_size_ORG_dat)
 class(tumour_size_ORG_mdl)
 class(tumour_size_ORG_par)
+class(tumour_size_ORG_task)
 
 # We can also just extract the data objects directly from the MDL:
-dat <- getDataObjects("tumour_size_25June2014_OAM.mdl")
+dat <- getDataObjects(modelFile)
 
 # This returns a list of objects, so we can extract the first one:
 dat <- dat[[1]]
 class(dat)
 
-# We can then import the data from the data object, so it can be plotted in R. Note 
-# that the .csv file needs to be in the working directory for this to work
-myData <- read(dat)
+# We can then import the data from the data object, so it can be plotted in R.
+# The sourceDir parameter specifies the directory in which the data files live,
+# in case they are not within the current directory.
+myData <- read(dat, sourceDir=parent.folder(modelFile))
 head(myData)
 
 # The data is now loaded into memory in R, so we can use any R functions we like:
@@ -54,10 +56,9 @@ boxplot(myData$AMT)
 xyplot(DV~TIME,groups=ID,data=myData,type="b",ylab="Tumour size",xlab="Time (weeks)")
 
 
-
 # We can also import the parameter objects and model objects:
-par <- getParameterObjects("tumour_size_25June2014_OAM.mdl")[[1]]
-mod <- getModelObjects("tumour_size_25June2014_OAM.mdl")[[1]]
+par <- getParameterObjects(modelFile)[[1]]
+mod <- getModelObjects(modelFile)[[1]]
 
 class(par)
 class(mod)
@@ -70,58 +71,42 @@ str(par)
 
 parUpdated <- update(par, block="STRUCTURAL", type="POP_SIZE0", with=list(value=99999))
 
-# Once we have updated this, we can create a MOG. A MOG requires one object only of
-# type dataObj, parObj, taskObj and mdlObj. Currently we do not have a method of
-# extracting task properties objects from MDL code, so for now we can create one manually. 
-# This would not usually be required, as we will be able to import directly from the 
-# mdl
 
-taskObj1 <- new("taskObj", 
-  IMPORT = list(nmadvan = list(target = "NMTRAN_CODE", name="ADVAN", 
-    param=list(model=0,trans=0, ncmt=0))),
-  DATA = list(),
-  PARAMETER = list(),
-  MODEL = list(tolrel=9),
-  TASK_FUNCTION = list(),
-  TARGET_CODE = list("$EST MAX=9990 NSIG=3 SIGL=9 NOABORT PRINT=1
-METHOD=CONDITIONAL INTERACTION
-MSFO=SIZE.MSF
-$COV")
-)
-
-class(taskObj1)
 
 # Now we have all 4 objects, we can create a MOG:
-myMog <- createMogObj(dataObj = dat, taskObj = taskObj1, mdlObj = mod, parObj = par)
+myMog <- createMogObj(dataObj = dat, mdlObj = mod, parObj = par, taskObj = tumour_size_ORG_task)
 
 
-################################################################################
 # We can then write the MOG back out to MDL:
-write(myMog, f="myMog")
+modelFileModified <- file.path(parent.folder(modelFile), "outputMog")
+write(myMog, f=modelFileModified)
+# ... we will use this later
 
-# We can then execute this mdl file using NONMEM:
-results <- estimate("myMog.mdl", target="NONMEM")
 
-# Alternatively, we can execute the MOG directly without creating the MDL file:
-results <- estimate(myMog, target="NONMEM")
-################################################################################
 
 # We can execute an mdl file directly:
-results <- estimate("tumour_size_25June2014_OAM.mdl", target="NONMEM")
+results1 <- estimate(modelFile, target="NONMEM")
 
 
 # For now, the results received will be in the form of an NMRun object 
 # (from the RNMImport package). In the future, a standardised object will be 
 # created which is compatible with other software (e.g. BUGS, PsN)
-class(results)
+class(results1)
+
+
+
+# We can try executing the amended mdl file that was written out earlier, using NONMEM:
+results2 <- estimate(paste0(modelFileModified,".mdl"), target="NONMEM")
+
+# Alternatively, we can execute the MOG directly without creating the MDL file:
+# TODO: Commented out at the moment since it currently writes an output.mdl file
+# to the current directory which is probably not what is desired
+#results3 <- estimate(myMog, target="NONMEM")
+
+
 
 # As part of the TEL package, there will be many functions for working with the 
 # standardised output object. These functions are currently in development, but as
 # an example we can use the getEstimationInfo function:
-getEstimationInfo(results)
-
-
-
-
-
+#getEstimationInfo(results1)
 
