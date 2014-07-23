5e850eaa71c681845df0f2cd5f670eaac996bb9d
khanley <khanley@mango-solutions.com>
2014-07-23 15:38:09 +0100

    Update generateRd.R file to allow auto-generation of the package namespace file. Only functions tagged with "@export" will be exported for the user.

--- generateRd.R
+++ generateRd.R
@@ -5,4 +5,4 @@ cmds = commandArgs(TRUE)
 pkgp = cmds[1]
 # avoid collate_roclet() - topological sort, very slow
 # avoid namespace_roclet() - we need to export everything
-roxygenize(pkgp,pkgp,roclets='rd')
+roxygenize(pkgp,pkgp,roclets=c('rd', 'namespace'))
