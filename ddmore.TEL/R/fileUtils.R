21696c42096815643bba34e7278fb990adef9801
Matthew Wise <mwise@mango-solutions.com>
2014-07-30 17:35:31 +0100

    DDMORE-800: Amendments to file copying back and forth: - Local -> Remote (FIS): Include any *.lst files in the file copying (for PsN with a pre-executed NONMEM run). - Remote (FIS) -> Local: Was copying back directly from MIF working dir to local. MIF might not be on same machine. Should be copying back from FIS working dir instead, excluding .fis and <jobID> dirs. This needs further tweaking though.

--- ddmore.TEL/R/fileUtils.R
+++ ddmore.TEL/R/fileUtils.R
@@ -21,11 +21,15 @@ TEL.prepareWorkingFolder <- function(modelfile=NULL, tmpdir=tempdir()) {
     
     inputs = file.path(srcdir, TEL.getInputs(modelfile, srcdir))
     
-    flist = c(modelfile, inputs)
-  
+	# Include any *.lst files in the file copying to remote too (for PsN with a pre-executed NONMEM run)
+	lstfiles <- file.path(srcdir, list.files(srcdir, pattern=".*\\.lst"))
+	
+    flist = c(modelfile, inputs, lstfiles)
+
     file.copy(flist, tempFolder)
     
   }
+  
   tempFolder
 }
 
@@ -42,56 +46,66 @@ TEL.import <- function(outputObject=NULL, target=NULL, clearUp=FALSE) {
 		jobDirectory <- job$workingDirectory
 	}
 	
-	workingFolder = file.path(jobDirectory, jobID)
+	workingFolder = file.path(jobDirectory) # should be FIS working directory not MIF working directory
 	
 	if (file.exists(workingFolder) == FALSE) {
-		cat("Job ", jobID, " already imported")
+		cat("Working directory ", workingFolder, " does not exist; Job ", jobID, " may have already been imported.\n")
 	} else {
 		# import the data
-		cat(sprintf('Copying the result data back to local machine for job ID: %s ...\n', jobID))
+		cat(sprintf('Copying the result data back to the local machine for job ID: %s ...\n', jobID))
 		cat(sprintf('From: %s to %s\n', workingFolder, target))
 		
-		if(file.exists(target)==FALSE) {
+		if (file.exists(target)==FALSE) {
 			dir.create(target)
 		}
-    
-		#  local.out.dir = sprintf("%s/%s", PROTO_HOME, jobID)
-		file.name.base = gsub('.mdl', '', modelfile)
-		file.name.base = gsub('.xml', '', file.name.base)
-		file.name.base = gsub('.ctl', '', file.name.base)
 		
-		#  dir.create(local.out.dir)
+		all.regular.files <- list.files(workingFolder, pattern="^[^.].*")
+		files.to.copy <- all.regular.files[-which(all.regular.files==jobID)] # Exclude the job directory (i.e. MIF working directory)
 		
-		#  file.copy(sprintf("%s/%s/%s/%s", MAPPED_DRIVE_NAME, jobID, 'inputs', paste(fn, sep = "")), local.out.dir)
+		file.copy(files.to.copy, target, recursive=TRUE)
 		
-        # Get anything that matches the filename from the job folder
-		similarnamedfilelist <- list.files(workingFolder, pattern=paste(file.name.base, "*.*", sep=""), full.names = TRUE)
 		
-		# Get any files that look like datafiles
-		# TODO: Determine what the actual data file(s) is
-		datafilelist <- list.files(workingFolder, "*.csv", full.names = TRUE)
 		
-		# Get any files that look like fit files
-		# TODO: Determine what the actual .fit file(s) is
-		fitfilelist <- list.files(workingFolder, "*.fit", full.names = TRUE)
+# WAS the following. TODO: Check how the above file copying compares to the below (specifically, finding similarly named files to the control file).    
+#		#  local.out.dir = sprintf("%s/%s", PROTO_HOME, jobID)
+#		file.name.base = gsub('.mdl', '', modelfile)
+#		file.name.base = gsub('.xml', '', file.name.base)
+#		file.name.base = gsub('.ctl', '', file.name.base)
+#		
+#		#  dir.create(local.out.dir)
+#		
+#		#  file.copy(sprintf("%s/%s/%s/%s", MAPPED_DRIVE_NAME, jobID, 'inputs', paste(fn, sep = "")), local.out.dir)
+#		
+#        # Get anything that matches the filename from the job folder
+#		similarnamedfilelist <- list.files(workingFolder, pattern=paste(file.name.base, "*.*", sep=""), full.names = TRUE)
+#		
+#		# Get any files that look like datafiles
+#		# TODO: Determine what the actual data file(s) is
+#		datafilelist <- list.files(workingFolder, "*.csv", full.names = TRUE)
+#		
+#		# Get any files that look like fit files
+#		# TODO: Determine what the actual .fit file(s) is
+#		fitfilelist <- list.files(workingFolder, "*.fit", full.names = TRUE)
+#		
+#		# Get any files that look like table files
+#		# TODO: Determine what the actual table files are
+#		tablefilelist <- list.files(workingFolder, pattern="^[a-z][a-z]tab[0-9]+$", full.names = TRUE)
+#    
+#        # Get lst file from NMFE execution
+#        lstFile = paste(workingFolder, "output.lst", sep="\\")
+#        
+#        fileslist <- c(similarnamedfilelist, datafilelist, fitfilelist, tablefilelist)
+#        
+#        if (file.exists(lstFile) == TRUE) {
+#            fileslist <- c(fileslist, lstFile)
+#        }
+#    
+#		file.copy(fileslist, target)
 		
-		# Get any files that look like table files
-		# TODO: Determine what the actual table files are
-		tablefilelist <- list.files(workingFolder, pattern="^[a-z][a-z]tab[0-9]+$", full.names = TRUE)
-    
-        # Get lst file from NMFE execution
-        lstFile = paste(workingFolder, "output.lst", sep="\\")
-        
-        fileslist <- c(similarnamedfilelist, datafilelist, fitfilelist, tablefilelist)
-        
-        if (file.exists(lstFile) == TRUE) {
-            fileslist <- c(fileslist, lstFile)
-        }
-    
-		file.copy(fileslist, target)
+		
 		
 		if (clearUp==TRUE) {
-			unlink(jobDirectory, recursive=TRUE)
+			unlink(workingFolder, recursive=TRUE)
 		}		
 		cat('Done.\n\n')
     
