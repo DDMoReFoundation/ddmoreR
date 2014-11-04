ccbde6c17d538c60518b45fe59c13038e51755fd
Matthew Wise <mwise@mango-solutions.com>
2014-11-04 18:28:08 +0000

    DDMORE-895: Update the update(parObj) func in line with recent changes.

--- ddmore.TEL/R/update.r
+++ ddmore.TEL/R/update.r
@@ -1,65 +1,60 @@
 ##############################################################
 #' update
 #'
-#' Updates an object of class parObj. It allows the user to specify new initial values, 
-#' bounds (lower, upper) and prior distributions. Typically this method is used to 
-#' update initial parameter values from current values to output values from an estimation step. 
-#' Optionally will pick up any changes to parameter objects via the Task Properties Object PARAMETER block.
+#' Updates an object of class parObj. It allows the user to specify new initial values,
+#' bounds (lower, upper) and prior distributions. Typically this method is used to update
+#' initial parameter values from current values to output values from an estimation step.
 #' 
-#' @usage update(object, block, type, with)
+#' @usage update(object, block, item, with)
 #'
 #' @param object An object of class parObj
-#' @param block Which block ("STRUCTURAL", "PRIOR" or "VARIABILITY") to update
-#' @param type Identifies which element within a block to update. Names correspond 
-#' to named list items within each block. E.g. value, lo, hi, matrix, prior.
-#' @param with Specifies the new values for the attribute identified through "block" and "type"
+#' @param block Which block ("STRUCTURAL", "VARIABILITY" or "PRIOR_PARAMETERS") to update
+#' @param item Identifies which element (e.g. variable) within a block to update;
+#'        corresponds to a named list item within the block
+#' @param with A named list specifying the attributes and their values, for the items
+#'        identified through "block" and "item"; the attributes must already exist
+#' @return The updated object of class parObj
 #'
 #' @examples
-#' ## Change the initial values for structural parameters
-#' ## Uses the "update" method
-#' ThamParObject2<-update(ThamParObject,
-#' 				block="STRUCTURAL"
-#'				type="value",
-#'				  with=list(POP_SIZE0 =7, 
-#'					    POP_TOVER =15, 
-#'					    POP_AE50e =10000, 
-#'					    POP_TEQ   =5)
-#'				)
-#'ThamParObject3<-update(ThamParObject,
-#'				block="STRUCTURAL"
-#'				type="fix",
-#'				with=list(POP_TOVER = T))
-#'
-#' @return An object of class parObj
+#' # Change the initial value for a structural parameter
+#' update(warfarinMOG@@parObj, "STRUCTURAL", "POP_V", list(value="2"))
+#' # Change the bounds of a variability parameter
+#' update(warfarinMOG@@parObj, "VARIABILITY", "CORR_PPV_CL_V", list(lo=-0.5, hi=+0.5))
+#' 
 #' @export
 #' @docType methods
 #' @rdname update-methods
 #' @include telClasses.R
 
-setGeneric("update", function(object, block, type, with){ 
+setGeneric("update", function(object, block, item, with) { 
   standardGeneric("update")
 })
 
 #' @rdname update-methods
 #' @aliases read,parObj,parObj-method
-setMethod("update", signature=signature(object="parObj"), function(object, block, type, with){
-  if(!all(block%in%c("STRUCTURAL", "PRIOR", "VARIABILITY"))){stop("Block provided is not one of 'STRUCTURAL', 'PRIOR' or 'VARIABILITY'")}
-  if(!all(type%in%names(eval(parse(text=paste0("object", "@", block)))))){stop("Type provided does not exist in given block.")}
-  if(!all(names(with)%in%names(eval(parse(text=paste0("object", "@", block, "$", type)))))){stop("Names given do not exist in given block and type.")}
+setMethod("update", signature=signature(object="parObj"), function(object, block, item, with) {
+			
+  if (!all(block%in%c("STRUCTURAL", "VARIABILITY", "PRIOR_PARAMETERS"))) {
+	  stop("Block provided is not one of \"STRUCTURAL\", \"VARIABILITY\" or \"PRIOR_PARAMETERS\"")
+  }
+  if (!all(item%in%names(eval(parse(text=paste0("object", "@", block)))))) {
+	  stop("Item (e.g. variable) provided does not exist in given block")
+  }
+  if (!all(names(with)%in%names(eval(parse(text=paste0("object", "@", block, "$", item)))))){
+	  stop("Names given do not exist in given block and item")
+  }
   
   # Create a string of the command, then evaluate it:
-  x <- paste0("object", "@", block, "$", type, "$", names(with), "<-", unlist(with))
-  eval(parse(text=x))
+  x <- 
+  eval(
+    parse(
+      text = paste0("object", "@", block, "$", item, "$", names(with), " <- ", unlist(with))
+    )
+  )
 
   # Return the updated object:
   return(object)
 
 })
-
-
-
-
-
-
 
 
