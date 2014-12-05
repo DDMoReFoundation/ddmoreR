7f7164e4aefbd175dace2aea28ef48bcbbf1529a
Matthew Wise <mwise@mango-solutions.com>
2014-12-05 10:56:15 +0000

    DDMORE-903: Override the message() func to print to stdout not stderr. Now, benign messages are not printed in red in the TEL-R console.

--- ddmore.TEL/R/utils.R
+++ ddmore.TEL/R/utils.R
@@ -1,3 +1,36 @@
+
+################################################################################
+#' Override the standard message() function to print to stdout not stderr
+#' in order that benign messages are not printed in red in the TEL-R console.
+#' 
+#' Taken from answer on:
+#' http://stackoverflow.com/questions/25306819/send-r-diagnostic-messages-to-stdout-instead-stderr
+#' 
+#' @export
+message <- function (..., domain = NULL, appendLF = TRUE) 
+{
+	args <- list(...)
+	cond <- if (length(args) == 1L && inherits(args[[1L]], "condition")) {
+				if (nargs() > 1L) 
+					warning("additional arguments ignored in message()")
+				args[[1L]]
+			}
+			else {
+				msg <- .makeMessage(..., domain = domain, appendLF = appendLF)
+				call <- sys.call()
+				simpleMessage(msg, call)
+			}
+	defaultHandler <- function(c) {
+		cat(conditionMessage(c), file = stdout(), sep = "")
+	}
+	withRestarts({
+				signalCondition(cond)
+				defaultHandler(cond)
+			}, muffleMessage = function() NULL)
+	invisible()
+}
+
+
 #' Pretty print a list object
 #'
 #'
