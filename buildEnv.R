3ad2fd14f2f568e26781dbf836dbfcd51cb7d512
Chris Musselle <cmusselle@mango-solutions.com>
2015-01-20 17:27:22 +0000

    DDMORE-976 Updated buildEnv.R and cmusselle.properties to use R 3.0.2 and packages built with R 3.0.2

--- buildEnv.R
+++ buildEnv.R
@@ -1,88 +1,57 @@
 ##############################
-# Author: khanley
-# Last modified: 02/06/2014
+# Author: cmusselle
 ##############################
-## Bring back the R_LIBS location
+
+## Fetch the R_LIBS location
 localLib <- Sys.getenv("R_LIBS")
 .libPaths(localLib)
 
+# Global options
+repo = "http://cran.rstudio.com/"
+dest.dir = "dependencies"
+
+dependencies = c("bitops", "brew", "digest", "Rcpp", "RCurl", "rjson", "RNMImport", 
+                 "roxygen2", "stringr",  "XML", "devtools")
 
 message("Checking for and installing dependencies...")
-if (!require(bitops, lib.loc=localLib)){
-  install.packages("./dependencies/bitops_1.0-6.zip", repos=NULL, 
-  lib=localLib, INSTALL_opts = "--no-lock")
-}
-if (!require(bitops, lib.loc=localLib)){
-  stop("bitops does not seem to be installed")
-}
-if (!require(RCurl, lib.loc=localLib)){
-  install.packages("./dependencies/RCurl_1.95-4.1.zip", repos=NULL, 
-  lib=localLib, INSTALL_opts = "--no-lock")
-}
-if (!require(RCurl, lib.loc=localLib)){
-  stop("RCurl does not seem to be installed")
-}
-if (!require(XML, lib.loc=localLib)){
-  install.packages("./dependencies/XML_3.98-1.1.zip", repos=NULL, 
-  lib=localLib, INSTALL_opts = "--no-lock")
-}
-if (!require(XML, lib.loc=localLib)){
-  stop("XML does not seem to be installed")
-}
 
-if (!require(rjson, lib.loc=localLib)){
-  install.packages("./dependencies/rjson_0.2.13.zip", repos=NULL, 
-  lib=localLib, INSTALL_opts = "--no-lock")
-}
-if (!require(rjson, lib.loc=localLib)){
-  stop("rjson does not seem to be installed")
-}
-if (!require(digest, lib.loc=localLib)){
-  install.packages("./dependencies/digest_0.6.4.zip", repos=NULL, 
-  lib=localLib, INSTALL_opts = "--no-lock")
-}
-if (!require(digest, lib.loc=localLib)){
-  stop("digest does not seem to be installed")
-}
-if (!require(roxygen2, lib.loc=localLib)){
-  install.packages("./dependencies/brew_1.0-6.zip", repos=NULL, 
-  lib=localLib, INSTALL_opts = "--no-lock")
-  
-  install.packages("./dependencies/Rcpp_0.11.1.zip", repos=NULL, 
-  lib=localLib, INSTALL_opts = "--no-lock")  
-  
-  install.packages("./dependencies/stringr_0.6.2.zip", repos=NULL, 
-  lib=localLib, INSTALL_opts = "--no-lock")
-  
-  install.packages("./dependencies/roxygen2_3.1.0.zip", repos=NULL, 
-  lib=localLib, INSTALL_opts = "--no-lock")
-}
-if (!require(RNMImport, lib.loc=localLib)){
-   install.packages("./dependencies/RNMImport_3.9.zip", repos=NULL, 
-  lib=localLib, INSTALL_opts = "--no-lock")
-}
+installFromLocalSourceIfPossible <- function(pkg.name, localLib, dest.dir) {
 
-if (!require(roxygen2, lib.loc=localLib)){
-  stop("roxygen2 does not seem to be installed")
-}
-if (!require(Rcpp, lib.loc=localLib)){
-  stop("Rcpp does not seem to be installed")
-}
-if (!require(stringr, lib.loc=localLib)){
-  stop("stringr does not seem to be installed")
-}
-if (!require(brew, lib.loc=localLib)){
-  stop("brew does not seem to be installed")
-}
-if (!require(RNMImport, lib.loc=localLib)){
-  stop("RNMImport does not seem to be installed")
-}
-if (!require(testthat, lib.loc=localLib)){
-  install.packages("./dependencies/testthat_0.7.2.99.tar.gz", repos=NULL, 
-  lib=localLib, INSTALL_opts = "--no-lock", type="source")
-}
-if (!require(testthat, lib.loc=localLib)){
-  stop("testthat does not seem to be installed")
+  if (!require(pkg.name, lib.loc=localLib, character.only = TRUE)){
+    
+    # Check if already downloaded
+    matching.files = list.files(dest.dir, pattern = paste0(pkg.name, "*"))
+    
+    if (length(matching.files) == 0) {
+      # Download latest from CRAN
+      install.packages(eval(pkg.name), lib=localLib, 
+          INSTALL_opts = "--no-lock", repos = repo, destdir = dest.dir, type="source")
+    
+    } else {
+      
+      if (length(matching.files) > 1) {
+        warning("Multiple versions of local source file detected")
+      }
+      
+      #  Install from local source
+      install.path = paste(dest.dir, matching.files[[1]], sep="/")
+      
+      install.packages(install.path, repos=NULL, 
+                      lib=localLib, INSTALL_opts = "--no-lock", 
+                      type="source")
+    }   
+    
+    if (!require(pkg.name, lib.loc=localLib, character.only = TRUE)){
+      stop(paste0(pkg.name, "failed to be installed"))
+    }
+  }
+} 
+
+
+for (pkg.name in dependencies) {
+
+  installFromLocalSourceIfPossible(pkg.name, localLib, dest.dir)
+
 }
 
 message("done")
