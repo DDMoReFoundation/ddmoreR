80f6e671dfbe4ed36df82da6a80488dc2b7c0072
Chris Musselle <cmusselle@mango-solutions.com>
2015-01-20 17:30:26 +0000

    Minor tweek to local scripts in development data R scripts

--- development data/parseXML2.R
+++ development data/parseXML2.R
@@ -3,39 +3,35 @@ rm(list=ls())
 
 # Setup imports and directory
 detach("package:DDMoRe.TEL", unload=TRUE)
-install.packages("C:/Users/cmusselle/Projects/DDmore/TEL-R/.__artefacts/DDMoRe.TEL_0.0.1.tar.gz", repos = NULL, type = "source")
+install.packages("C:/Users/cmusselle/Projects/DDmore/TEL-R/.__artefacts/DDMoRe.TEL_0.0.2.tar.gz", repos = NULL, type = "source")
 require("DDMoRe.TEL")
 require("XML")
 
-setwd("C:/Users/cmusselle/Projects/DDmore/TEL-R/development data")
+setwd("C:/Users/cmusselle/Projects/DDmore/TEL-R/ddmore.TEL")
 
-source("../ddmore.TEL/R/xmlParsers.R")
+source("ddmore.TEL/R/xmlParsers.R")
+
+C:\Users\cmusselle\Projects\DDmore\TEL-R\ddmore.TEL\inst\tests\data\PharmMLSO\HandCoded
 
 # Generate Blank SO object 
 SOObject = createSOObject()
 
 # Get a reference to the root node in the xml doc
-root = xmlRoot(xmlTreeParse("development data\\MONOLIX_SO\\Warfarin-ODE-latest.SO.xml"))
+#root = xmlRoot(xmlTreeParse("development data\\MONOLIX_SO\\Warfarin-ODE-latest.SO.xml"))
+root = xmlRoot(xmlTreeParse("inst\\tests\\data\\PharmMLSO\\HandCoded\\warfarin_PK_ODE_SO_FULL.xml"))
+root = xmlRoot(xmlTreeParse("inst\\tests\\data\\PharmMLSO\\MachineGenerated\\Warfarin-ODE-latest.SO.xml"))
 
-# Update the namespace to use 'd' as the defualt. Must be done to use Xpath expressions with namespaces
-ns = xmlNamespaceDefinitions(root, simplify = TRUE)
-defaultNS = 'd'
-names(ns)[1] <- defaultNS
 
 # Fetch List of SOBlock elements
-sink("NUL")
-SOlist = xpathApply(root, "//d:SOBlock", namespaces=ns)
-sink()
+SOBlockList = root[names(root) == "SOBlock"]
 
 # Assumes only one SOBlock for now!
-stopifnot(length(SOlist) == 1)
+stopifnot(length(SOBlockList) == 1)
 
-### Future for loop to start here
-
-RootSONode = SOlist[[1]]
+### Future for loop to start here if multiple SO blocks
 
 # Fetch all Components of the SO object that are defined
-SOChildren = xmlChildren(RootSONode)
+SOChildren <- xmlChildren(SOBlockList[[1]])
 
 # Execute Parsers
 SOObject = ParseToolSettings(SOObject, SOChildren$ToolSettings)
@@ -55,9 +51,34 @@ SOObject = ParsePredictions(SOObject, SOChildren$Estimation[["Predictions"]])
 
 
 
+
+SOChildren = xmlChildren(root[["SOBlock"]])
+
+PIE = root[["SOBlock"]][["Estimation"]][["PrecisionIndividualEstimates"]]
+
+LogNormalDistribution 
+
+
+
+
 MLEDataSet = root[[2]][["Estimation"]][["PopulationEstimates"]][["MLE"]]
 
-ParseDataSet(MLEDataSet)
+MLEDataSet = root[[2]][["Estimation"]][["PopulationEstimates"]][["MLE"]]
+
+
+
+TaskInformationNode = root[["SOBlock"]][["TaskInformation"]]
+
+SOObject = ParseTaskInformation(SOObject, TaskInformationNode)
+
+
+
+ResidualsNode = root[["SOBlock"]][["Estimation"]][["Residuals"]]
+
+
+
+SOObject = ParseResiduals(SOObject, ResidualsNode)
+
 
 
 #Node = xpathApply(root, "/d:SO/d:SOBlock/d:Estimation/d:PrecisionPopulationEstimates", namespaces=ns)
