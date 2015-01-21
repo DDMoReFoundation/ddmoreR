36a64b11f4c892b68cd8077bfa2cd9673186e17b
Chris Musselle <cmusselle@mango-solutions.com>
2015-01-21 18:09:12 +0000

    Revert "DDMORE-976 Updated buildEnv.R and cmusselle.properties to use R 3.0.2 and packages built with R 3.0.2"
    This reverts commit 3ad2fd14f2f568e26781dbf836dbfcd51cb7d512.
    
    Also Added first batch of Unit tests for LoadSOObject

--- ddmore.TEL/inst/tests/testthat/test.LoadSOObject.R
+++ ddmore.TEL/inst/tests/testthat/test.LoadSOObject.R
@@ -1,8 +1,10 @@
 library("DDMoRe.TEL")
 library("XML")
-context("Loading SOObject from PharmML")
+require("methods")
 
-test_that("Loading SOObject from latest hand coded PharmML SO", {
+context("Loading SOObject from hand coded PharmML")
+
+test_that("Loading SOObject from latest hand coded PharmML SO fills expected slots", {
 
   # Clear workspace. 
   rm(list=ls())
@@ -12,7 +14,271 @@ test_that("Loading SOObject from latest hand coded PharmML SO", {
   # Load in SO
   SOObject = LoadSOObject(hand.coded.data.path)
   
-  expect_that( isS4(SOObject), TRUE, label = "Object is S4")
+  # Test is S4
+  expect_true(isS4(SOObject), info = "Object is S4", )
+  
+  # Test Slots have been populated #
+  # ------------------------------ #
+  
+  # ToolSettings
+  expect_true(length(SOObject@ToolSettings) > 0 , info = "Object is not empty", )
+  
+  # RawResults
+  expect_true(
+    length(SOObject@RawResults@DataFiles) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@RawResults@GraphicsFiles) > 0 , 
+    info = "Slot is not empty", )
+  
+  #TaskInformation
+  expect_false(
+    all(sapply(SOObject@TaskInformation$Messages, is.null)), 
+    info = "All Message slots are not empty", )
+  
+  expect_true(
+    length(names(SOObject@TaskInformation)[names(SOObject@TaskInformation) != "Messages"]) > 0, 
+    info = "Messages is not the only element present")
+  
+  # Estimates
+  expect_true(
+    length(SOObject@Estimation@PopulationEstimates) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@PrecisionPopulationEstimates) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@IndividualEstimates) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@PrecisionIndividualEstimates) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@Residuals) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@Predictions) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@Likelihood) > 0 , 
+    info = "Slot is not empty", )
+  
    
 })
   
+context("Loading SOObject from machine coded PharmML (Warfarin-ODE-latest.SO.xml)")
+
+test_that("Loading SOObject from latest Machine Generated PharmML SO (Warfarin-ODE-latest.SO.xml)", {
+  
+  # Clear workspace. 
+  rm(list=ls())
+  
+  machine.coded.data.path = system.file("tests/data/PharmMLSO/MachineGenerated/Warfarin-ODE-latest.SO.xml",  package = "DDMoRe.TEL")
+  
+  # Load in SO
+  SOObject = LoadSOObject(machine.coded.data.path)
+  
+  # Test is S4
+  expect_true(isS4(SOObject), info = "Object is S4", )
+  
+  # Test Slots have been populated #
+  # ------------------------------ #
+  
+  # ToolSettings
+  expect_true(length(SOObject@ToolSettings) > 0 , info = "Object is not empty", )
+  
+  # RawResults
+  expect_true(
+    length(SOObject@RawResults@DataFiles) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@RawResults@GraphicsFiles) > 0 , 
+    info = "Slot is not empty", )
+  
+  #TaskInformation
+  expect_false(
+    all(sapply(SOObject@TaskInformation$Messages, is.null)), 
+    info = "All Message slots are not empty", )
+  
+  expect_true(
+    length(names(SOObject@TaskInformation)[names(SOObject@TaskInformation) != "Messages"]) > 0, 
+    info = "Messages is not the only element present")
+  
+  # Estimates
+  expect_true(
+    length(SOObject@Estimation@PopulationEstimates) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@PrecisionPopulationEstimates) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@IndividualEstimates) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@PrecisionIndividualEstimates) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@Residuals) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@Predictions) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@Likelihood) > 0 , 
+    info = "Slot is not empty", )
+  
+})
+
+context("Loading SOObject from machine coded PharmML (run1.SO.xml)")
+
+test_that("Loading SOObject from latest Machine Generated PharmML SO (run1.SO.xml)", {
+  
+  # Clear workspace. 
+  rm(list=ls())
+  
+  machine.coded.data.path = system.file("tests/data/PharmMLSO/MachineGenerated/run1.SO.xml",  package = "DDMoRe.TEL")
+  
+  # Load in SO
+  SOObject = LoadSOObject(machine.coded.data.path)
+  
+  # Test is S4
+  expect_true(isS4(SOObject), info = "Object is S4", )
+  
+  # Test Slots have been populated #
+  # ------------------------------ #
+  
+  # ToolSettings
+  expect_true(length(SOObject@ToolSettings) > 0 , info = "Object is not empty", )
+  
+  # RawResults
+  expect_true(
+    length(SOObject@RawResults@DataFiles) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@RawResults@GraphicsFiles) > 0 , 
+    info = "Slot is not empty", )
+  
+  #TaskInformation
+  expect_false(
+    all(sapply(SOObject@TaskInformation$Messages, is.null)), 
+    info = "All Message slots are not empty", )
+  
+  expect_true(
+    length(names(SOObject@TaskInformation)[names(SOObject@TaskInformation) != "Messages"]) > 0, 
+    info = "Messages is not the only element present")
+  
+  # Estimates
+  expect_true(
+    length(SOObject@Estimation@PopulationEstimates) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@PrecisionPopulationEstimates) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@IndividualEstimates) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@PrecisionIndividualEstimates) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@Residuals) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@Predictions) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@Likelihood) > 0 , 
+    info = "Slot is not empty", )
+  
+})
+
+context("Loading SOObject from machine coded PharmML (pheno.SO.xml)")
+
+test_that("Loading SOObject from latest Machine Generated PharmML SO (pheno.SO.xml)", {
+  
+  # Clear workspace. 
+  rm(list=ls())
+  
+  machine.coded.data.path = system.file("tests/data/PharmMLSO/MachineGenerated/pheno.SO.xml",  package = "DDMoRe.TEL")
+  
+  # Load in SO
+  SOObject = LoadSOObject(machine.coded.data.path)
+  
+  # Test is S4
+  expect_true(isS4(SOObject), info = "Object is S4", )
+  
+  # Test Slots have been populated #
+  # ------------------------------ #
+  
+  # ToolSettings
+  expect_true(length(SOObject@ToolSettings) > 0 , info = "Object is not empty", )
+  
+  # RawResults
+  expect_true(
+    length(SOObject@RawResults@DataFiles) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@RawResults@GraphicsFiles) > 0 , 
+    info = "Slot is not empty", )
+  
+  #TaskInformation
+  expect_false(
+    all(sapply(SOObject@TaskInformation$Messages, is.null)), 
+    info = "All Message slots are not empty", )
+  
+  expect_true(
+    length(names(SOObject@TaskInformation)[names(SOObject@TaskInformation) != "Messages"]) > 0, 
+    info = "Messages is not the only element present")
+  
+  # Estimates
+  expect_true(
+    length(SOObject@Estimation@PopulationEstimates) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@PrecisionPopulationEstimates) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@IndividualEstimates) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@PrecisionIndividualEstimates) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@Residuals) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@Predictions) > 0 , 
+    info = "Slot is not empty", )
+  
+  expect_true(
+    length(SOObject@Estimation@Likelihood) > 0 , 
+    info = "Slot is not empty", )
+  
+})
