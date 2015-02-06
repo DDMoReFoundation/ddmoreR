56e9a25d413f6c8318237c89a3a04c12db7dce53
Matthew Wise <mwise@mango-solutions.com>
2015-02-06 12:37:57 +0000

    DDMORE-1036,DDMORE-1039: Mainly, allow multiple SO import in execution: 1. Provide a "multiple" parameter to importSO() function, that if true calls LoadSOObjects() rather than LoadSOObject() 2. Provide a "extraInputFiles" parameter to prepareWorkingFolder() function, that is used in the derivation of the additional files to copy to the job working directory 3. Refactor out an execute() function from the existing estimate() function 4. execute() provided with additional 'internal' parameters extraInputFileExts, extraInputFiles, importSO, importMultipleSO; the first two being passed on to prepareWorkingFolder() and the latter two used in the call to importSO() 5. psnWrappers.R functions updated to call the new internal execute() function rather than the public API function estimate() - but Kajsa will need to make further changes here re the above changes

--- ddmore.TEL/R/import.R
+++ ddmore.TEL/R/import.R
@@ -123,10 +123,10 @@ TEL.importFiles <- function(submission, target=file.path(submission$sourceDirect
 
 
 ################################################################################
-#' Import results as Standard Output object.
+#' Import results as Standard Output object(s)
 #' 
 #' Import the results retrieved from an execution of an MDL file, into an object
-#' of class \linkS4class{StandardOutputObject}.
+#' of class \linkS4class{StandardOutputObject}, or a list thereof.
 #' 
 #' @param submission Named list containing information relating to the
 #'        submission of an execution request:
@@ -148,21 +148,30 @@ TEL.importFiles <- function(submission, target=file.path(submission$sourceDirect
 #'              then this import into Standard Output object will not work.
 #'          \item{\code{requestId}} - Unique identifier for the submission request.
 #'        }
-#' @return Object of class \linkS4class{StandardOutputObject}.
-#'
+#' @param multiple Whether multiple SOBlocks are expected in the SO XML results file.
+#'        Default is FALSE. Note that if FALSE and multiple SOBlocks are encountered,
+#'        an error will be thrown.
+#' @return Object of class \linkS4class{StandardOutputObject}, or if \code{multiple}
+#'         is TRUE, then a list of such objects.
+#' 
 #' @author mwise
 #' 
 #' @export
 #' 
+#' @include LoadSOObject.R
 #' @include StandardOutputObject.R
 
-TEL.importSO <- function(submission) {
+TEL.importSO <- function(submission, multiple=FALSE) {
 	
 	soXMLFileName <- paste0(file_path_sans_ext(submission$modelFile), ".SO.xml")
 	soXMLFilePath <- file.path(submission$resultsDir, soXMLFileName)
 
 	if (class(soXMLFilePath) == "character" && file.exists(soXMLFilePath)) {
-		LoadSOObject(soXMLFilePath)
+		if (multiple) {
+			LoadSOObjects(soXMLFilePath)
+		} else {
+			LoadSOObject(soXMLFilePath)
+		}
 	}
 	else {
 		stop(
