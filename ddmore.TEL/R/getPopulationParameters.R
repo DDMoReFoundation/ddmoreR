167dc86b574cff37726083c5959c5a232cee8574
cmusselle <cmusselle@mango-solutions.com>
2015-06-24 14:35:07 +0100

    DDMORE-1138 Amended quote rendering in documentation

--- ddmore.TEL/R/getPopulationParameters.R
+++ ddmore.TEL/R/getPopulationParameters.R
@@ -9,19 +9,18 @@
 #' estimates of the STRUCTURAL and RANDOM_VARIABILITY parameters along with any 
 #' associated measures of uncertainty (Std Dev) and interval estimates if these 
 #' are available.
-#' Usage
 #' 
 #' @param SOObject an object of class StandardOutputObject, the output from an 
 #'    estimation task.
 #' @param type character string determining which parameters to return. 
-#'    Options are "structural", "variability", "all" (default).
+#'    Options are \dQuote{structural}, \dQuote{variability}, \dQuote{all} (default).
 #' @param what character vector determining what values to return:
 #'  \itemize{
-#'    \item "estimates" – returns point estimates for the parameters.
-#'    \item "precision" – returns variability / uncertainty estimates for the 
+#'    \item \dQuote{estimates} – returns point estimates for the parameters.
+#'    \item \dQuote{precision} – returns variability / uncertainty estimates for the 
 #'        parameters.
-#'    \item "intervals" - returns interval estimates for the parameters. 
-#'    \item"all" – returns all of the above in a table.}
+#'    \item \dQuote{intervals} - returns interval estimates for the parameters. 
+#'    \item\dQuote{all} – returns all of the above in a table.}
 #'
 #' @param keep.only character string determining which central tendency statistic to use 
 #'      for the estimate when multiple are present. Only applicable to Bayesian which
@@ -31,14 +30,14 @@
 #' estimation method used and how these are populated – either directly from the
 #' estimation task output or subsequently via other methods e.g. bootstrapping. 
 #' The function will provide suitable names for the columns depending on the 
-#' methods used. So for example, although the argument for "what" accepts 
-#' "precision" this may mean SE assuming Normality, Bootstrap SE, SD from MCMC 
+#' methods used. So for example, although the argument for \dQuote{what} accepts 
+#' \dQuote{precision} this may mean SE assuming Normality, Bootstrap SE, SD from MCMC 
 #' or SAEM estimation methods.
 #' 
-#' @return If only returning "estimates" or "precision" then a named vector of 
-#' real values. If returning "intervalEstimates" or "All" then a data frame 
-#' containing one row for each parameter. Columns are "Estimate", "Precision", 
-#' "Lower", "Upper", "Shrinkage"
+#' @return If only returning \dQuote{estimates} or \dQuote{precision} then a named vector of 
+#' real values. If returning \dQuote{intervalEstimates} or \dQuote{All} then a data frame 
+#' containing one row for each parameter. Columns are \dQuote{Estimate}, \dQuote{Precision}, 
+#' \dQuote{Lower}, \dQuote{Upper}, \dQuote{Shrinkage}
 #' 
 #' @examples getPopulationParameters(object, type="all", what="all")
 #'
