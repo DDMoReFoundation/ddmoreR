f474c53bb6ef66a2eb7bf2f5fc75f2dc9dba2d5e
Matthew Wise <mwise@mango-solutions.com>
2015-10-23 08:47:15 +0100

    SF #186: Add note to update() function docu

--- ddmore.TEL/R/update.r
+++ ddmore.TEL/R/update.r
@@ -18,7 +18,13 @@
 #'        boolean attribute values should be enclosed in double quotes as per string
 #'        attribute values
 #' @return The updated object of class parObj
-#'
+#' 
+#' @note When trying to update multiple attributes across multiple variables e.g. via
+#'       \code{p <- update(p, 'STRUCTURAL', names(p@STRUCTURAL), list(value=0.5565, lo=0.6656))}
+#'       then this won't neccessarily update the correct values, since the names of the
+#'       attributes aren't checked in this case, so which attributes get which values is
+#'       arbitrary. This is raised as a SourceForge ticket #186.
+#'  
 #' @examples
 #' # Change the initial value for a structural parameter
 #' update(warfarinMOG@@parObj, "STRUCTURAL", "POP_V", list(value="2"))
