2252c2f8bccf59851b6e346a822aa3352e16c4c3
cmusselle <cmusselle@mango-solutions.com>
2015-10-29 11:15:27 +0000

    Revert "Revert "DDMORE-1514 Renamed read, update and write as per https://sourceforge.net/p/ddmore/tickets/321""
    This reverts commit 40e9f47d87204719adb988f62564cb672c690e54.

--- ddmore.TEL/R/updateParObj.R
+++ ddmore.TEL/R/updateParObj.R
@@ -1,12 +1,12 @@
 ##############################################################
-#' update
+#' updateParObj
 #'
 #' Updates an object of class \code{\linkS4class{parObj}}. It allows the user to specify
 #' new initial values, bounds (lower, upper), prior distributions, and whether to fix or
 #' unfix a variable. Typically this method is used to update initial parameter values
 #' from current values to output values from an estimation step.
 #' 
-#' @usage update(object, block, item, with)
+#' @usage updateParObj(object, block, item, with)
 #'
 #' @param object An object of class \code{\linkS4class{parObj}}
 #' @param block Which block ("STRUCTURAL", "VARIABILITY" or "DECLARED_VARIABLES") to update
@@ -20,31 +20,31 @@
 #' @return The updated object of class parObj
 #' 
 #' @note When trying to update multiple attributes across multiple variables e.g. via
-#'       \code{p <- update(p, 'STRUCTURAL', names(p@STRUCTURAL), list(value=0.5565, lo=0.6656))}
+#'       \code{p <- updateParObj(p, 'STRUCTURAL', names(p@STRUCTURAL), list(value=0.5565, lo=0.6656))}
 #'       then this won't neccessarily update the correct values, since the names of the
 #'       attributes aren't checked in this case, so which attributes get which values is
 #'       arbitrary. This is raised as a SourceForge ticket #186.
 #'  
 #' @examples
 #' # Change the initial value for a structural parameter
-#' update(warfarinMOG@@parObj, "STRUCTURAL", "POP_V", list(value="2"))
+#' updateParObj(warfarinMOG@@parObj, "STRUCTURAL", "POP_V", list(value="2"))
 #' # Change the bounds of a variability parameter
-#' update(warfarinMOG@@parObj, "VARIABILITY", "CORR_PPV_CL_V", list(lo=-0.5, hi=+0.5))
+#' updateParObj(warfarinMOG@@parObj, "VARIABILITY", "CORR_PPV_CL_V", list(lo=-0.5, hi=+0.5))
 #' # Fix the value of a parameter
-#' update(nockMOG@parObj, "VARIABILITY", "SIGMA", list(fix="true"))
+#' updateParObj(nockMOG@parObj, "VARIABILITY", "SIGMA", list(fix="true"))
 #' 
 #' @export
 #' @docType methods
-#' @rdname update-methods
+#' @rdname updateParObj-methods
 #' @include telClasses.R
 
-setGeneric("update", function(object, block, item, with) { 
-  standardGeneric("update")
+setGeneric("updateParObj", function(object, block, item, with) { 
+  standardGeneric("updateParObj")
 })
 
-#' @rdname update-methods
+#' @rdname updateParObj-methods
 #' @aliases read,parObj,parObj-method
-setMethod("update", signature=signature(object="parObj"), function(object, block, item, with) {
+setMethod("updateParObj", signature=signature(object="parObj"), function(object, block, item, with) {
 			
   if (!all(block%in%c("STRUCTURAL", "VARIABILITY", "PRIOR_PARAMETERS"))) {
 	  stop("Block provided is not one of \"STRUCTURAL\", \"VARIABILITY\" or \"PRIOR_PARAMETERS\"")
