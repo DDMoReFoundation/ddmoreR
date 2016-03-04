d5b5fbe7476523b57626a073ebc32d7424376d29
Matthew Wise <mwise@mango-solutions.com>
2016-03-04 09:10:52 +0000

    DDMORE-1607: ParseElement() now creates DataSet instances itself. Previously ParseElement() was returning a pair of (description, data) dataframes and then the appropriate object constructors in Estimation-Class.R were creating and populating the DataSet instances each time one was needed. This change simplifies this, reducing duplicated code, and making the parsing more generic.

--- ddmore.TEL/R/LoadSOObject.R
+++ ddmore.TEL/R/LoadSOObject.R
@@ -245,12 +245,8 @@ createSOObjectFromXMLSOBlock <- function(soBlock) {
 		
 		if ("Predictions" %in% names(SOChildren[["Estimation"]])){
 			#SOObject <- ParsePredictions(SOObject, SOChildren[["Estimation"]][["Predictions"]])
-			# Table expected - TODO call specific function
-			L <- ParseElement(SOChildren[["Estimation"]][["Predictions"]])
-			SOObject@Estimation@Predictions <- DataSet()
-			# TODO: Do this on DataSet() constructor instead
-			SOObject@Estimation@Predictions@description <- L$description
-			SOObject@Estimation@Predictions@data <- L$data
+			# Table expected - TODO call specific function ?
+			SOObject@Estimation@Predictions <- ParseElement(SOChildren[["Estimation"]][["Predictions"]])
 			messageList[["parsed"]] <- append(messageList[["parsed"]], "Estimation:Predictions")
 		} else {
 			messageList[["skipped"]] <- append(messageList[["skipped"]], "Estimation:Predictions")
@@ -267,6 +263,11 @@ createSOObjectFromXMLSOBlock <- function(soBlock) {
 	} else {
 		messageList[["skipped"]] <- append(messageList[["skipped"]], "Estimation")
 	}
+	
+	
+	# TODO remove this obviously
+	return(SOObject)
+	
 	
 	if ("Simulation" %in% names(SOChildren)){
 		
